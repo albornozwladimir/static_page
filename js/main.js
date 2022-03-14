5fcc4dcddd3a7599f84d566fb8ba894d994afe21
chbron <albornoz.wladimir@gmail.com>
2022-03-14 13:36:05 -0300

    Minimalismo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const medias = document.querySelector("#socialMedia")
+
+if (medias){
+	medias.innerHTML = 
+	` <a class="button button-linkedin"
+		 href="https://linkedin.com/in/wladimir-albornoz-6aa24a112" target='_blank'>
+		 <img class="icon" src="assets/icons/linkedin.svg"> Work Network</a>
+	  <a class="button button-github"
+		 href="https://github.com/albornozwladimir" target='_blank'>
+		 <img class="icon" src="assets/icons/github.svg"> Codes Repo</a>
+	  <a class="button button-medium"
+		 href="mailto:wladimir.albornoz@usach.cl">
+		 <img class="icon" src="assets/icons/mail.svg">Mail</a> <br>
+	`
+}
